53500a0c82bb943c589b5f9926064020943891c8
karunakar <mkr1011@rediffmail.com>
2024-10-27 15:58:22 +0530

    adding files

---
+++ calci.js
@@ -0,0 +1,13 @@
+function takeInput(input){
+    document.getElementById('numInp').value += input;
+}
+function allClear(){
+    document.getElementById('numInp').value = '';
+}
+function calculate(){
+    const output = document.getElementById('numInp');
+    output.value = eval(output.value);
+}
+function del(){
+    
+}
